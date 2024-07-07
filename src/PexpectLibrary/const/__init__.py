6624f1f8af2e4bebef2a207e2224ced9655eb54b
Michael_Zuo <michael_zuoxy@foxmail.com>
2024-07-07 14:23:27 +0800

    Add SearchModeType

--- src/PexpectLibrary/const/__init__.py
+++ src/PexpectLibrary/const/__init__.py
@@ -1,3 +1,11 @@
+from enum import Enum
+
 from PexpectLibrary.utils.env import get_int_env
 
-PEXPECT_TIMEOUT_FOR_LAUNCH_COMMAND = get_int_env('PEXPECT_TIMEOUT_FOR_LAUNCH_COMMAND',15)
+PEXPECT_TIMEOUT_FOR_LAUNCH_COMMAND = get_int_env('PEXPECT_TIMEOUT_FOR_LAUNCH_COMMAND', 15)
+
+
+class SearchModeType(int, Enum):
+    HEAD = 0
+    TAIL = 1
+    MIDDLE = 2
